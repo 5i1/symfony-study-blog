7ee6eb0bede0352670d536c38c84b4e6604d448b
Thomas Kanzig <thomas.kanzig@gmail.com>
2019-06-05 20:53:12 +0200

    Add category entity and post_category for many to many

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -55,6 +57,16 @@ class Post
      * @ORM\Column(type="string", length=300, nullable=true)
      */
     private $url_photo;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Category")
+     */
+    private $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -153,6 +165,32 @@ class Post
     public function setUrlPhoto(?string $url_photo): self
     {
         $this->url_photo = $url_photo;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Category[]
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->contains($category)) {
+            $this->categories->removeElement($category);
+        }
 
         return $this;
     }
