4bfc9be1c727ea252a4ac2dd58508d0c96b33d8b
Thomas Känzig <thomas.kaenzig@massiveart.com>
2019-06-26 20:18:45 +0200

    Resolve issues with jQuery undefined

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -9,13 +9,11 @@
 require('../css/admin.scss');
 
 // Import JS from AdminLTE.
-import 'admin-lte/bower_components/bootstrap/dist/js/bootstrap.min.js';
-import 'admin-lte/plugins/iCheck/icheck.min.js';
-import 'admin-lte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js';
-import 'admin-lte/bower_components/fastclick/lib/fastclick.js';
-import 'admin-lte/dist/js/adminlte.min';
+require('admin-lte/bower_components/bootstrap/dist/js/bootstrap');
+require('admin-lte/plugins/iCheck/icheck');
+require('admin-lte/bower_components/jquery-slimscroll/jquery.slimscroll');
+require('admin-lte/bower_components/fastclick/lib/fastclick.js');
+require('admin-lte/dist/js/adminlte.min');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
-
-
