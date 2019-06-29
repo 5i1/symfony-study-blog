913fa4819a115cb3b0341410cb323e11536fefac
Thomas Känzig <thomas.kaenzig@massiveart.com>
2019-06-29 19:26:44 +0200

    Added paginator in post list with knp paginator.

--- src/Controller/Admin/PostController.php
+++ src/Controller/Admin/PostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Post;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
@@ -11,10 +14,28 @@ class PostController extends AbstractController
     /**
      * @Route("/admin/posts", name="admin_post_index")
      */
-    public function index()
+    public function index(Request $request, PaginatorInterface $paginator)
     {
 
-        return $this->render('admin/post/index.html.twig');
+        // Retrieve the entity manager of Doctrine
+        $em = $this->getDoctrine()->getManager();
+
+        // Get some repository of data, in our case we have an Appointments entity
+        $appointmentsRepository = $em->getRepository(Post::class);
+
+        // Find all the data on the Appointments table, filter your query as you need
+        $queryBuilder = $appointmentsRepository->createQueryBuilder('p')
+            ->getQuery();
+
+        $pagination = $paginator->paginate(
+            $queryBuilder, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            3/*limit per page*/
+        );
+
+        return $this->render('admin/post/index.html.twig', [
+            'posts' => $pagination,
+        ]);
     }
 
     /**
