b05603535add3c36fdbe638d15f018a91f185988
Thomas Känzig <thomas.kaenzig@massiveart.com>
2019-06-30 23:13:48 +0200

    Added form with save data.

--- src/Controller/Admin/PostController.php
+++ src/Controller/Admin/PostController.php
@@ -7,6 +7,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
+use App\Form\PostType;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Post;
 
 class PostController extends AbstractController
 {
@@ -33,9 +36,32 @@ class PostController extends AbstractController
     /**
      * @Route("/admin/post/add", name="admin_post_add")
      */
-    public function add()
+    public function add(EntityManagerInterface $em, Request $request)
     {
 
-        return $this->render('admin/post/add.html.twig');
+        // Create the form based on the FormType we need.
+        $post = new Post();
+        $postForm = $this->createForm(PostType::class, $post);
+
+        // Ask the form to handle the current request.
+        $postForm->handleRequest($request);
+
+        if ($postForm->isSubmitted() && $postForm->isValid()) {
+            // Process the submitted & validated data retrieved in $form->getData()
+            // Form handling is *here*.
+
+            $post->setSlug('example-of-slug');
+            $post->setUser($this->getUser());
+            $post->setCreated(new \DateTime());
+
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_post_index');
+        }
+
+        return $this->render('admin/post/add.html.twig', [
+            'postForm' => $postForm->createView()
+        ]);
     }
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -6,22 +6,24 @@ use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('text')
-            ->add('created')
-            ->add('modified')
-            ->add('deleted')
-            ->add('description')
-            ->add('url_photo')
-            ->add('categories')
-            ->add('user')
+            ->add('title',  TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('text', TextareaType::class)
+            ->add('url_photo',
+                  FileType::class,
+                  ['label' => 'Image file for the post banner' ]
+            )
+            ->add('Save Post', SubmitType::class)
         ;
     }
 
