35b681051b9bf4bcf60b69b388fbfbab76fdbc99
Thomas Kanzig <thomas.kanzig@gmail.com>
2019-08-23 00:23:20 +0200

    Configured for restful API.

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,6 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
---
+++ src/Controller/Rest/MediaController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Rest;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+
+/***
+ * Moviecontroller.
+ * @Route("/api", name="api_")
+ */
+class MediaController extends AbstractFOSRestController
+{
+    /***
+     * Upload media.
+     *
+     * @Rest\Get("/media/upload")
+     *
+     */
+    public function uploadAction(Request $request)
+    {
+        $success = false;
+        $errors = [];
+        $message = 'Post creation failed. Please try again.';
+
+        $data = [
+            'success' => $success,
+            'message' => $message,
+            'errors' => $errors
+        ];
+
+        return View::create($data, Response::HTTP_CREATED);
+    }
+}
